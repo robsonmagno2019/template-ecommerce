881366f8589755cf3a6e95d41252e0e78f752da2
Robson Magno <robsonmagnosistemas@gmail.com>
2024-12-25 23:50:29 -0400

    Configurado JWJ na API - Aula 06

---
+++ src/services/Template.Identidade.API/Extentions/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Template.Identidade.API.Extentions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
