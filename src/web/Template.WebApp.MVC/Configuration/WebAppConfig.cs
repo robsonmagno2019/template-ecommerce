9b9258071bfb6ae245b12f76179d687a3c9f9c46
Robson Magno <robsonmagnosistemas@gmail.com>
2024-12-31 13:44:36 -0400

    Adicionado config.

---
+++ src/web/Template.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,38 @@
+﻿namespace Template.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.UseEndpoints(Endpoint =>
+            {
+                Endpoint.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
