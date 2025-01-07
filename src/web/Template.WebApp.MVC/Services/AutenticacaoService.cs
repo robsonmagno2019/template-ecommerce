edc883fab3e0fbfb80642f5a63271d0a97fc82d4
Robson Magno <robsonmagnosistemas@gmail.com>
2025-01-07 18:37:54 -0400

    Usando HttpService - Aplicação Web - Aula 05

---
+++ src/web/Template.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Text.Json;
+using Template.WebApp.MVC.Models;
+
+namespace Template.WebApp.MVC.Services
+{
+    public class AutenticacaoService : IAutenticacaoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AutenticacaoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Login(UsuarioLogin usuarioLogin)
+        {
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(usuarioLogin),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:44316/api/identidade/autenticar", loginContent);
+
+            return await Task.FromResult("");
+        }
+
+        public async Task<string> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            var registroContent = new StringContent(
+                JsonSerializer.Serialize(usuarioRegistro),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:44316/api/identidade/autenticar", registroContent);
+
+            return await Task.FromResult("");
+        }
+    }
+}
---
+++ src/web/Template.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -0,0 +1,10 @@
+﻿using Template.WebApp.MVC.Models;
+
+namespace Template.WebApp.MVC.Services
+{
+    public interface IAutenticacaoService
+    {
+        Task<string> Login(UsuarioLogin usuarioLogin);
+        Task<string> Registro(UsuarioRegistro usuarioRegistro);
+    }
+}
